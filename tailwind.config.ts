ea92184cd7c4b7832de4a6617b247c193d4c5e38
Okoro Joshua <Joshua65676@gmail.com>
2024-12-10 09:30:58 +0100

    first next commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,10 +7,35 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    container: {
+      center: true,
+      padding: "1.5rem",
+      screens: {
+        "2xl": "1280px",
+      },
+    },
     extend: {
+      fontFamily: {
+        kumbh: ['Kumbh Sans', 'sans-serif'],
+      },
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        Orange: 'hsl(26, 100%, 55%)',
+        BgOrange: 'rgba(255, 172, 106, 255)',
+        PaleOrange: 'hsl(25, 100%, 94%)',
+        DarkBlue: 'hsl(220, 13%, 13%)',
+        Grayishblue: 'hsl(219, 9%, 45%)',
+        GrayishBlue: 'hsl(220, 14%, 75%)',
+        LightGrayishBlue: 'hsl(223, 64%, 98%)',
+        White: 'hsl(0, 0%, 100%)',
+        Black : 'hsl(0, 0%, 0%)'
+      },
+      screens: {
+        sm: '300px',
+        mx: '350px',
+        xm: '410px',
+        md: '768px',
+        lg: '876px',
+        xl: '1440px',
       },
     },
   },
