ab0ae113c0816e3a979fbfa7227ac750bd212a79
Okoro Joshua <Joshua65676@gmail.com>
2024-12-15 18:33:17 +0100

    update next commit

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -1,5 +1,7 @@
-import Image from 'next/image';
-// import { use } from 'next/navigation';
+import Image from "next/image";
+import { FaStar } from "react-icons/fa6";
+import { MdAddShoppingCart } from "react-icons/md";
+import { Button } from "@/components/ui/Button";
 
 type Product = {
   _id: number;
@@ -33,16 +35,80 @@ const ProductDetails = async ({ params }: { params: { id: string } }) => {
   }
 
   return (
-    <div className="container mx-auto p-4">
-      <Image src={product.image} alt={product.title} width={400} height={400} className="w-full h-64 object-cover rounded-md" />
-      <h1 className="mt-4 text-2xl font-bold">{product.title}</h1>
-      <p className="mt-2 text-gray-700">{product.description}</p>
-      <p className="mt-2 text-gray-700">Price: ${product.price}</p>
-      <p className="mt-2 text-gray-500 line-through">Old Price: ${product.oldPrice}</p>
-      <p className="mt-2 text-gray-700">Brand: {product.brand}</p>
-      <p className="mt-2 text-gray-700">Sold Out: {product.soldOut}</p>
-      <p className="mt-2 text-gray-700">Rating: {product.rating}</p>
-    </div>
+    <section className="container max-w-7xl mx-auto w-full">
+      <main className="flex flex-row justify-between gap-[10rem] items-center  p-5 py-32">
+        {/* Image */}
+        <div className="w-[200rem] h-full">
+          <Image
+            src={product.image}
+            alt={product.title}
+            width={1000}
+            height={1000}
+            className="h-[32rem]"
+          />
+        </div>
+        {/* Details */}
+        <div className="flex flex-col gap-5">
+          {/* Title */}
+          <div className="flex flex-col gap-4">
+            <div className="">
+              <span className="font-kumbh text-Grayishblue font-semibold uppercase tracking-widest text-[14px]">
+                {product.brand}
+              </span>
+            </div>
+            <div className="">
+              <h1 className="font-kumbh text-Black font-bold md:text-4xl mx:text-3xl leading-[50px]">
+                {product.title}
+              </h1>
+            </div>
+          </div>
+          {/* Discrition */}
+          <div className="">
+            <p className="font-mono md:font-medium mx:font-bold md:text-[15px] mx:text-[12.5px] text-GrayishBlue leading-[26.2px] -tracking-[2.5%]">
+              {product.description}
+            </p>
+          </div>
+          {/* soldout&rating&Isnew */}
+          <div className="flex flex-col gap-3">
+            <span className="text-Black">{product.isNew}</span>
+            <div className="flex flex-row gap-5">
+              <span className="flex flex-row gap-1 font-mono font-semibold">
+                {product.soldOut}
+                <span className="text-GrayishBlue font-serif text-sm pt-[3px]">
+                  SoldOut
+                </span>
+              </span>
+              <span className="flex flex-row gap-1 font-mono font-semibold">
+                <FaStar className="mt-1 text-Orange" />
+                {product.rating}
+              </span>
+            </div>
+          </div>
+          {/* Price */}
+          <div className="">
+            <div className="flex flex-row gap-5">
+              <span className="font-bold text-2xl font-kumbh">
+                ${product.price}
+              </span>
+              <span className="text-lg font-mono font-medium text-Grayishblue line-through pt-[2px]">
+                ${product.oldPrice}
+              </span>
+            </div>
+          </div>
+          {/* AddToCartButton */}
+          <div className="">
+            <Button className="bg-Orange border-Orange md:w-56 mx:w-full rounded-xl hover:bg-BgOrange">
+              <div className="flex flex-row gap-5">
+                <MdAddShoppingCart className="h-8 w-6" />
+                <span className="text-Black font-bold  font-kumbh text-base pt-1">
+                  Add To Cart
+                </span>
+              </div>
+            </Button>
+          </div>
+        </div>
+      </main>
+    </section>
   );
 };
 
